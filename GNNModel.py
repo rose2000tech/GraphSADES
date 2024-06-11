a222523f7f35bd89e0d3b5d7e412a15e0ee162aa
rose2000tech <pengping19871212@163.COM>
2024-06-11 15:09:59 +0800

    Update GNNModel.py

--- GNNModel.py
+++ GNNModel.py
@@ -8,26 +8,26 @@ import pdb
 from torch.nn import Linear
 
 
-# 定义 GCN 模型
+# Defining the GCN model
 class GNNModel(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, num_classes):
         super(GNNModel, self).__init__()
         torch.manual_seed(1234)
-        self.conv1 = GCNConv(input_dim, 4)  # 只需定义好输入特征和输出特征即可
+        self.conv1 = GCNConv(input_dim, 4)  # Simply define the input features and output features
         self.conv2 = GCNConv(4, 4)
         self.conv3 = GCNConv(4, 2)
         self.classifier = Linear(2, num_classes)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        h = self.conv1(x, edge_index)  # 输入特征与邻接矩阵(注意格式，上面那种)
+        h = self.conv1(x, edge_index)  # Input feature and adjacency matrix (note the format, the kind above)
 
         h = h.tanh()
         h = self.conv2(h, edge_index)
         h = h.tanh()
         h = self.conv3(h, edge_index)
         h = h.tanh()
-        # 分类层
+        # classification layer
         out = self.classifier(h)
         return out, h
 
